325a14a6ca1f82c32824964b6f44903eda8851a3
Hossein <Hossein@GeneBook-MacBook-Pro.local>
2015-08-11 14:32:55 -0400

    first commit

---
+++ Bayinfer/Bayinfer/listOfFunction.h
@@ -0,0 +1,110 @@
+//
+//  listOfFunction.h
+//  ReadFgInC
+//
+//  Created by Hossein on 2014-06-11.
+//  Copyright (c) 2014 Hossein. All rights reserved.
+
+
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <math.h>
+#include <sys/time.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+
+#ifndef listOfFunction
+#define listOfFunction
+#include "perfect.h"
+
+
+#ifndef STANDARD
+#include "standard.h"
+#endif
+#ifndef PHASH
+#include "phash.h"
+#endif
+
+#ifndef EPS
+#define  EPS 1e-300 ;
+#endif
+#ifndef logEPS
+#define  logEPS -30 ;
+#endif
+#ifndef LargeNumber
+#define  LargeNumber 1e300;
+#endif
+#ifndef LargeNegNumber
+#define  LargeNegNumber -1e300;
+#endif
+
+
+
+// define the struct node for message passing
+struct Node
+{
+    int numAdj;     // number of neighbors
+    int numComb;   // number of CPD combinations; zero for variable nodes
+    int *adjNodes; // hash Ids of neighbors
+    int *oIdx;     // indexes of outbound messages
+    int *iIdx;     // indexes of inbound messages
+    double * beliefs; // beliefs of nodes
+};
+
+
+typedef struct Node Node ;
+
+
+
+
+double inAbsolute(double i);
+int cstring_cmp(const void *a, const void *b);
+int uniq(char *a[], int len);
+int readCharFile(char * lines[],char *fileToRead,int maxbufflen);
+void rad_normaliseC(double *T, int N);
+void maxVector(double *maxV, int *maxIdx, double  *v, int length_v);
+int ExtractNonUniqNodeNum(char *filename);
+int ExtractNonUniqNodelist(char *filename,char  ** nodeName);
+
+
+void LogSplashLBP(double **factorarray,int **array,int *numComb,Node *AlarmNet,int num_state,int N,int Nv,int lu,int *iteration);
+int Splash(double *W, Node *AlarmNet,int *Sigma, int v, int maxW,int maxSplashSize,double *resBelief);
+
+int LogRoundRobinSplashLBP(double **factorarray,int **array,Node *AlarmNet,double* u,double* u_old,int num_state,int N,int Nv,int lu);
+void RoundRobinSplashLBP(double **factorarray, int **array,int *numComb,Node *AlarmNet,int N,int Nv,int lu,int * iteration);
+
+void belief_resBelief_Splash(double **factorarray, int **array,int *numComb,Node*AlarmNet,int N,int Nv,int lu,int *iteration);
+
+void LogRBP(double **factorarray, int **array,int *numComb,Node *AlarmNet,int num_state,int N,int Nv,int lu,int *iteration);
+
+int **makeCPTindex(Node *pGraph,int Nf,int Nv,int num_state);
+int ReadObservedData(char *filename,  double **obs_values, int **obs_Ids,int *numSample);
+
+int pairwiseTofactorgraph(char *readpairwisefilename,char * writefactorgraphfilename,int nstate);
+int  hashSourceTargetNodes(char *readpairwisefilename);
+int mod (int a, int b);
+void normalizeCPD(double * f, double *CPD,int numComb,int num_state,int flag);
+int FactorgraphFile_To_NodeStructures(char *pathway, Node *pGraph,int num_state,int Nv,int Nf);
+
+void inputBasedModifiedCPT(Node *pGraph,double ** factorarray, int *obs_values,int *obs_Ids, int **cpt, int Nv,int numObs,int  flag );
+
+void doLBPinference(char *pathway,char * obs_data,char *nodepost,int num_state);
+
+
+int reaction_logic_to_factorgraph(char *readreactionlogicpathways,char * writefactorgraphfilename,int nstate);
+
+int ReadMultipleVisibleSets(char *filename,  double **obs_values, int **obs_Ids,int *numSample);
+
+void hash_graph_node_IDs(char *readreactionlogicpathways);
+
+void learning_discrete_BayNet(char *input4columns,char * pathway,char *obs_data,char *nodepost, int num_state, int max_num_repeat, double LLchangeLimit, int MAPflag);
+
+
+#endif
---
+++ hash_graph_node_IDs/hash_graph_node_IDs/main.c
@@ -0,0 +1,54 @@
+//
+//  main.c
+//  hash_graph_node_IDs
+//
+//  Created by Hossein on 2015-07-30.
+//  Copyright (c) 2015 Hossein. All rights reserved.
+//
+
+
+
+
+
+
+#include "hash_graph_node_IDs.h"
+
+int main(void)
+{
+    
+    
+       clock_t begin, end; /* to measure run time */
+    
+    double time_spent; /* to measure run time */
+    
+    /* Intializes random number generator */
+    /* see for detail : http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)Randomization.html  */
+    unsigned int randval;
+    FILE *f;
+    
+    f = fopen("/dev/random", "r");
+    fread(&randval, sizeof(randval), 1, f);
+    fclose(f);
+    
+    srand(randval);
+    
+    begin =clock();
+    
+    
+    
+    
+    char input4columns[20000]= "/Users/Hossein/Documents/MATLAB/work/KEGG_parser/reaction_logic_pathways/JE_Network.txt";
+    
+    
+    /*read name of nodes and hash them*/
+    
+     hash_graph_node_IDs(input4columns);
+    
+    
+    /* report running time*/
+    end = clock();
+    time_spent = (double)(end-begin) / CLOCKS_PER_SEC;
+    printf(" Total time (sec) :  %f\n",time_spent);
+    return 0;
+}
+
