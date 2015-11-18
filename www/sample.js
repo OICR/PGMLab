63e86c8c50819d33b42519090c01c617e30f5c96
Elizabeth Y <elizabethany@Elizabeths-MacBook-Pro.local>
2015-11-18 13:59:42 -0500

    changing folder structure

---
+++ www/sample.js
@@ -0,0 +1,43 @@
+    var graph = {
+	"nodes": [{
+            "name": "A",
+                "group": 1
+        }, {
+            "name": "B",
+                "group": 2
+        }, {
+            "name": "C",
+                "group": 1
+        }, {
+            "name": "D",
+                "group": 2
+        }, {
+            "name": "E",
+                "group": 2
+        }, {
+            "name": "F",
+                "group": 2
+        }],
+            "links": [{
+            "source": 0,
+                "target": 2,
+                "value": 1
+        }, {
+            "source": 0,
+                "target": 3,
+                "value": 1
+        }, {
+            "source": 1,
+                "target": 3,
+                "value": 1
+        }, {
+            "source": 3,
+                "target": 5,
+		"value": 1
+        }, {
+            "source": 4,
+                "target": 5,
+                "value": 1
+        }]
+    };
+
