d5656762c771866c564c493a39c8db6b61226e0d
Cristina Baciu <cbaciu@olm-cbaciu.oicr.on.ca>
2016-04-05 10:50:40 -0400

    made it so that when using joel file we are considering an one to many relationship between gene_id to entity_id

--- bin/convert_snv_to_obs.pl
+++ bin/convert_snv_to_obs.pl
@@ -8,10 +8,12 @@ use autodie;
 use feature qw(say);
 use Data::Dumper;
 
-my $snv_file_path = "/Users/cbaciu/Desktop/PGMLaB-old/data/GISTIC_v2_run/integrated_anal/SNV/202_donors_snv.txt";
-my $pi_file_path = "/Users/cbaciu/Desktop/4paths_28mar16/Cell_Cycle_Checkpoints/Cell_Cycle_Checkpoints.pi";
-my $db_id_to_name_mapping = "/Users/cbaciu/Desktop/PGMLaB-old/db_id_to_name_mapping_feb22.16.txt";
-my $sample_list ="/Users/cbaciu/Desktop/PGMLaB-old/data/GISTIC_v2_run/integrated_anal/SNV/202_common_BART_Gistic";
+#perl convert_snv_to_obs.pl <snv_file_path> <pi_file_path> <db_id_to_name_mapping> <sample_list>   > out.obs
+
+my $snv_file_path = $ARGV[0];#"/Users/cbaciu/Desktop/PGMLaB-old/data/GISTIC_v2_run/integrated_anal/SNV/202_donors_snv.txt";
+my $pi_file_path = $ARGV[1]; #"/Users/cbaciu/Desktop/4paths_28mar16/Cell_Cycle_Checkpoints/Cell_Cycle_Checkpoints.pi";
+my $db_id_to_name_mapping = $ARGV[2]; #"/Users/cbaciu/Desktop/PGMLaB-old/db_id_to_name_mapping_feb22.16.txt";
+my $sample_list = $ARGV[3]; #"/Users/cbaciu/Desktop/PGMLaB-old/data/GISTIC_v2_run/integrated_anal/SNV/202_common_BART_Gistic.txt";
 
 #Get the rectome ids to names; 
 open(my $fh_db_id, "<", $db_id_to_name_mapping);
@@ -25,7 +27,12 @@ while (my $reactome_map = <$fh_db_id>) {
 
      if ( $entity_type eq 'ReferenceGeneProduct') {
           $reactome_id_to_entity_name{$reactome_id} = $entity_name;
-          $entity_name_to_reactome_id{$entity_name} = $reactome_id;
+          unless (exists $entity_name_to_reactome_id{$entity_name}) {
+              $entity_name_to_reactome_id{$entity_name} = [$entity_name];
+          }
+          else {
+              push @{$entity_name_to_reactome_id{$entity_name}}, $reactome_id;
+          }
      }
 } 
 
@@ -58,15 +65,16 @@ open(my $fh_snv, "<", $snv_file_path);
 
 <$fh_snv>; # getting the header
 
-my (%sample_to_mutation, %genes_observed, $gene, $sample, $gene_id);
+my (%sample_to_mutation, %genes_observed, $gene, $sample, $gene_ids);
 while (my $mutation = <$fh_snv>) {
     chomp $mutation;
     ($gene, $sample) = split "\t", $mutation;
-    $gene_id = $entity_name_to_reactome_id{$gene};
-    $gene_id //= -1;
-    if (exists $pi_genes{$gene_id}) {
-        $sample_to_mutation{$sample}{$gene_id} = 1;
-        $genes_observed{$gene_id} = 1;
+    $gene_ids = $entity_name_to_reactome_id{$gene};
+    foreach my $gene_id (@$gene_ids) {
+        if (exists $pi_genes{$gene_id}) {
+            $sample_to_mutation{$sample}{$gene_id} = 1;
+            $genes_observed{$gene_id} = 1;
+        }
     }
 }
 
@@ -78,7 +86,6 @@ close($fh_snv);
 ######
 
 #getting samples form sample file
-
 
 open(my $fh_samples, "<", $sample_list);
 
