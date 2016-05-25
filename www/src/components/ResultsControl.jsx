6003044485656970f55c1878f245ec8005188647
Martin Pham <mdpham@uwaterloo.ca>
2016-05-25 12:44:39 -0400

    ResultsControl: component

---
+++ www/src/components/ResultsControl.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class ResultsControl extends React.Component {
+  render(){
+    return (
+      <div className="section">
+        <a className="waves-effect waves-light btn">Run</a>
+        <a className="waves-effect waves-light btn">Results</a>
+      </div>
+    );
+  }
+}
