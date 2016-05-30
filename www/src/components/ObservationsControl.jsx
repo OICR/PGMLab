42a321c86f760194db6663890e8c8afa641b2a60
Martin Pham <mdpham@uwaterloo.ca>
2016-05-30 10:37:01 -0400

    Observations/PathwaysControl: changed paging across selected into select field

--- www/src/components/ObservationsControl.jsx
+++ www/src/components/ObservationsControl.jsx
@@ -2,62 +2,45 @@ import React from 'react'
 
 import {NodeList} from "./NodeList.jsx";
 import {NodeItem} from "./NodeItem.jsx";
+import {SelectField, MenuItem} from "material-ui";
 
 var classNames = require("classnames");
 
 export class ObservationsControl extends React.Component {
   constructor(props){
     super(props);
-
-    this.shiftActiveObservation = this.shiftActiveObservation.bind(this);
+    
+    this.handleObservationChange = this.handleObservationChange.bind(this);
     this.header = this.header.bind(this);
   }
-  shiftActiveObservation(shift){
-    // Find currently active observation's closest neighbour in selectedObservations by comparing index distances
-    const direction = (distance)=>{
-      switch (shift) {
-        case "left": return distance < 0;
-        case "right": return distance > 0;
-      };
-    };
-    const selectedObservations = this.props.selectedObservations;
-    const currentActivatedIndex = selectedObservations.get("Active");
-    const nextActivated = selectedObservations.get("Indices").reduce((best,index)=>{
-      const distance = index-currentActivatedIndex;
-      const correctDirection = direction(distance);
-      const closer = Math.abs(distance) < Math.abs(best.distance);
-      switch (correctDirection && closer) {
-        case true: return {index,distance};
-        default: return best;
-      };
-    }, {index:currentActivatedIndex,distance:Infinity});
-    // console.log(nextActivated, selectedObservations.get("Indices"));
-    switch (nextActivated.distance === Infinity) {
-      case true: return;
-      default: this.props.setActiveObservation(nextActivated.index);
+
+  handleObservationChange(newActiveIndex){
+    if (this.props.selectedObservations.get("Active") !== newActiveIndex) {
+      this.props.setActiveObservation(newActiveIndex);
     };
   }
 
   // RENDERING
   header(){
-    const currentActivatedIndex = this.props.selectedObservations.get("Active");
+    const selectedObservations = this.props.selectedObservations;
+    const observationIndices = selectedObservations.get("Indices");
+    const activeObservationIndex = selectedObservations.get("Active");
+    const observationItems = observationIndices.map(i => {
+      return (
+        <MenuItem key={i} value={i}  primaryText={`Observation ${i}`} />
+      );
+    })
     return (
-      <ul className="pagination">
-        <li className="waves-effect blue lighten-5" onClick={()=>{this.shiftActiveObservation("left")}}>
-          <a><i className="material-icons">chevron_left</i></a>
-        </li>
-        <li className="waves-effect blue lighten-5" onClick={()=>{this.shiftActiveObservation("right")}}>
-          <a><i className="material-icons">chevron_right</i></a>
-        </li>
-        <li><strong>{`Observation: ${currentActivatedIndex}`}</strong></li>
-      </ul>
+      <SelectField  value={activeObservationIndex}
+                    onChange={(evt,idx,val)=>{this.handleObservationChange(val)}}
+                    fullWidth={true} floatingLabelText={"Active Observation"}
+                    children={observationItems} />
     );
   }
   render(){
     const noPad={paddingBottom:"0px", paddingTop:"0px"};
     return (
       <div className="section" style={noPad}>
-        <h5>Active Observation</h5>
         {this.header()}
         <NodeList activeType="Observation"
                   selectedObservationSet={this.props.selectedObservationSet}
--- www/src/components/PathwaysControl.jsx
+++ www/src/components/PathwaysControl.jsx
@@ -2,60 +2,38 @@ import React from 'react';
 
 import {NodeList} from "./NodeList.jsx";
 import {NodeItem} from "./NodeItem.jsx";
+import {SelectField, MenuItem} from "material-ui";
 
 export class PathwaysControl extends React.Component {
   constructor(props){
     super(props);
 
-    this.shiftActivePathway = this.shiftActivePathway.bind(this);
+    this.handlePathwayChange = this.handlePathwayChange.bind(this);
     this.header = this.header.bind(this);
   }
 
-  shiftActivePathway(shift){
-    // Sort selectedPathway IDs
-    const selectedPathways = this.props.selectedPathways;
-    const currentSelected = selectedPathways.get("Pathways");
-    const sortedSelectedPathways = this.props.pathways.reduce((sorted, pathway)=>{
-      if (currentSelected.includes(pathway.id)) {sorted.push(pathway);};
-      return sorted;
-    }, []);
-    // Find currently active pathway's closest neighbour by index distance
-    const direction = (distance)=>{
-      switch (shift) {
-        case "left": return distance < 0;
-        case "right": return distance > 0;
-      };
-    };
-    const currentActivatedIndex = currentSelected.indexOf(selectedPathways.get("Active").id);
-    const nextActivated = sortedSelectedPathways.reduce((best,pathway,index)=>{
-      const distance = index-currentActivatedIndex;
-      const correctDirection = direction(distance);
-      const closer = Math.abs(distance) < Math.abs(best.distance);
-      switch (correctDirection && closer) {
-        case true: return {pathway,distance};
-        default: return best;
-      };
-    }, {pathway:selectedPathways.get("Active"), distance:Infinity});
-    // Set new activePathway
-    switch (nextActivated.distance === Infinity) {
-      case true: return;
-      default: this.props.setActivePathway(nextActivated.pathway);
+  handlePathwayChange(pathwayID){
+    if (pathwayID !== this.props.selectedPathways.get("Active").id) {
+      const pathway = this.props.pathways.find(p=>p.id===pathwayID);
+      this.props.setActivePathway(pathway);
     };
   }
 
   // RENDERING
   header(){
-    const activePathway = this.props.selectedPathways.get("Active");
+    const selectedPathways = this.props.selectedPathways;
+    const currentSelected = selectedPathways.get("Pathways");
+    const allPathways = this.props.pathways;
+    const pathwayItems = currentSelected.map((id)=>{
+      const found = allPathways.find(p=>p.id === id);
+      return <MenuItem  key={found.id} value={found.id}
+                        primaryText={`${found.name}`} label={`${found.name}`}/>
+    });
     return (
-      <ul className="pagination">
-        <li className="waves-effect blue lighten-5" onClick={()=>{this.shiftActivePathway("left")}}>
-          <a><i className="material-icons">chevron_left</i></a>
-        </li>
-        <li className="waves-effect blue lighten-5" onClick={()=>{this.shiftActivePathway("right")}}>
-          <a><i className="material-icons">chevron_right</i></a>
-        </li>
-        <li><strong>{`${activePathway.name}`}</strong></li>
-      </ul>
+      <SelectField  value={selectedPathways.get("Active").id}
+                    onChange={(evt,idx,val)=>{this.handlePathwayChange(val)}}
+                    fullWidth={true} floatingLabelText={"Active Pathway"}
+                    children={pathwayItems} />
     );
   }
   render(){
@@ -63,7 +41,6 @@ export class PathwaysControl extends React.Component {
     const noPad={paddingBottom:"0px", paddingTop:"0px"};
     return (
       <div className="section" style={noPad}>
-        <h5>Active Pathway</h5>
         {this.header()}
         <NodeList activeType="Pathway"
                   selectedObservationSet={this.props.selectedObservationSet}
