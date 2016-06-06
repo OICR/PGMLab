1de2968490a2c87619a72de081ed885e905af8b5
Martin Pham <mdpham@uwaterloo.ca>
2016-06-06 10:11:03 -0400

    Header, Footer navigation

--- www/src/components/Header.jsx
+++ www/src/components/Header.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 
+var classNames = require("classnames");
 export class Header extends React.Component {
+  handleSetTab(tab){
+    this.props.setTab(tab);
+    this.tabs = this.tabs.bind(this);
+  }
+
+  tabs(){
+    return ["Run", "Results"].map(tab => (
+      <li key={tab} className={classNames({"active":tab===this.props.tab})}>
+        <a href="#!" onClick={()=>{this.handleSetTab(tab)}}>{tab}</a>
+      </li>
+    ));
+  }
   render(){
     return (
       <header className="row">
         <nav className="light-blue lighten-1" role="navigation">
           <div className="nav-wrapper">
-            <a style={{fontSize:"50px"}}  clasName="brand-logo center" href="#">
-              <span style={{color: "#2e7d32"}}>PGM</span><span style={{color:"#9c27b0"}}>Lab</span>
+            <a className="brand-logo center" href="#!" style={{fontSize:"50px"}}>
+              PGMBio
             </a>
+            <ul id="nav-mobile" className="left hide-on-med-and-down" style={{width:"175px"}}>
+              {this.tabs()}
+            </ul>
             <ul id="nav-mobile" className="right hide-on-med-and-down" style={{width:"175px"}}>
-                <li><a href="http://github.com/OICR/PGMLab-GUI">Code</a></li>
-                <li><a href="https://github.com/OICR/PGMLab-GUI/wiki">Wiki</a></li>
+                <li><a href="http://github.com/OICR/PGMLab">Git</a></li>
+                <li><a href="https://github.com/OICR/PGMLab/wiki">Wiki</a></li>
             </ul>
           </div>
         </nav>
