a9d5f9061f2519578a7015c8283ec6ced5b4f6e1
Martin Pham <mdpham@uwaterloo.ca>
2016-06-07 08:50:38 -0400

    InCHlib example

--- www/src/components/HeatMapPanel.jsx
+++ www/src/components/HeatMapPanel.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 
+import InChlib from "biojs-vis-inchlib";
+// import example from "json!./example.json";
+
 export class HeatMapPanel extends React.Component {
   constructor(props){
     super(props);
+    // console.log(InChlib);
   }
-
+  componentDidMount(){
+    var heatmap = new InChlib({
+      target: "heatmapContainer",
+      metadata: false,
+      column_metadata: false,
+      max_height: 1200,
+      width: 800,
+      meatmap_colors: "Greens",
+      metadata_colors: "Reds"
+    });
+    // console.log(example);
+    // heatmap.read_data(test);
+    // heatmap.draw();
+  }
   render(){
     return (
-      <span>HEATMAP</span>
+      <div>
+        <div id="heatmapContainer"></div>
+      </div>
     );
   }
 }
