fcfe12b2840a5573e515bb913b7335d046fb9794
Martin Pham <mdpham@uwaterloo.ca>
2016-06-14 09:25:07 -0400

    backend inchlib

--- www/klein/server_web.py
+++ www/klein/server_web.py
@@ -1,7 +1,12 @@
-from twisted.logger import Logger
+from klein import Klein
+
+from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import Application
+from twisted.logger import Logger
 
 import os, subprocess, shutil, json, string, uuid
+import inchlib_clust
+print inchlib_clust
 
 app = Application()
 log = Logger()
@@ -10,16 +15,17 @@ cwd = os.getcwd()
 hosted_data = cwd+"/../../data/reactome_template/"
 
 def system_call(command):
-    p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=true)
+    p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
     return p.stdout.read()
 
 
 def getReactomeFolder(pathwayID):
     f = open(hosted_data+"folder_to_reactome_id.txt")
     for line in f:
+        kv = line.split("\t")
         if kv[0] == pathwayID:
-            kv[1]
-        return None
+            return kv[1]
+    return None
 
 # REACTOME
 @app.register("reactome.pathway.get")
@@ -37,8 +43,6 @@ def getPathwayList():
     jsonData = open(hosted_data+"pathways.json").read()
     pathwayList = json.loads(jsonData)
     return pathwayList
-print getPathwayList()
-
 
 
 # PGMLab
@@ -46,7 +50,7 @@ def createPairwiseInteractionFile(runPath, pathway):
     filePath = runPath + "/pathway.pi"
     pi = open(filePath, "w")
     nodeCount = len(pathway)
-    pi.write(str(numberOfNodes)+"\n\n")
+    pi.write(str(nodeCount)+"\n\n")
     for interaction in pathway:
         pi.write(str(interaction["source"]) + "\t")
         pi.write(str(interaction["target"]) + "\t")
@@ -59,7 +63,7 @@ def createObservationFile(runPath, observationSet):
     obs = open(filePath, "w")
     observations = observationSet["observations"]
     obsCount = len(observations)
-    obs.write(str(numberObs)+"\n")
+    obs.write(str(obsCount)+"\n")
     for observation in observations:
         nodeCount = len(observation)
         obs.write(str(nodeCount)+"\n")
@@ -90,7 +94,7 @@ def readPosteriorProbabilityFile(runPath):
     return postProbs
 
 @app.register("pgmlab.inference.run")
-def runInference(pathway, observatioNSet, pathways, options):
+def runInference(pathway, observationSet, pathwaySet, options):
     runID = str(uuid.uuid4())
     cwd = os.getcwd()
     tmpPath = cwd + "/../../tmp/"
@@ -98,14 +102,16 @@ def runInference(pathway, observatioNSet, pathways, options):
     os.mkdir(runPath)
 
     posteriorProbabilities = {}
-    for id,pathway in pathways.items():
+    for id,pathway in pathwaySet.items():
         if "pairwiseInteractions" in pathway:
             links = pathway["pairwiseInteractions"]["links"]
-            print "links", links
+            # print "links", links
+            print "pathway.pairwiseInteractions.links"
         else:
             reactomePathway = getPathway(id)
             links = reactomePathway["links"]
-            print 'reactome', reactomePathway
+            print "reactome pathway"
+            # print 'reactome', reactomePathway
         createPairwiseInteractionFile(runPath, links)
         generateFactorgraph(runPath)
         createObservationFile(runPath, observationSet)
@@ -119,31 +125,20 @@ def runInference(pathway, observatioNSet, pathways, options):
         "runID":runID,
         "submitDateTime":options["submitDateTime"],
         "posteriorProbabilities":posteriorProbabilities,
-        "posteriorProbabilitiesSet":posteriorProbabilitiesSet,
         "observationSet":observationSet,
-        "selectedPathways":pathways
+        "pathwaySet":pathwaySet
     }
 
 # InCHlib
 # InCHlib clustering returns formatted JSON data to pass to js library
 # Expects array of posterior probability sets {nodeID: [s1,s2,s3] ...}
-def inchlibCSV(runPath, fileName, stateData, obsCount):
-    filePath = runPath+"/"+fileName+".csv"
-    csv = open(filePath, "w")
-    csv.write(reduce(lambda prev,curr: prev+",Observation "+str(curr), range(obsCount), "id"))
-    csv.write("\n")
-    for nodeID, obsStates in stateData.items():
-        csv.write((
-            str(nodeID)+
-            reduce(lambda prev,curr: prev+","+str(curr),obsStates,"")+
-            "\n"))
-    csv.close()
 def sortHeatmapData(heatmapData):
     state1 = {}
     state2 = {}
     state3 = {}
     stateD = {}
-    for obsIdx, observation in enumerate(posteriorProbsData):
+    # print heatmapData["posteriorProbsData"]["posteriorProbabilitiesSet"]
+    for obsIdx, observation in enumerate(heatmapData["posteriorProbsData"]["posteriorProbabilitiesSet"]):
         for nodeID,states in observation.items():
             if nodeID in state1:
                 state1[nodeID].append(states[0])
@@ -159,16 +154,28 @@ def sortHeatmapData(heatmapData):
             else:
                 state3[nodeID] = [False]*obsIdx + [states[2]]
             if nodeID in stateD:
-                stateD[nodeID].append('SOMEMAX')
+                stateD[nodeID].append(states.index(max(states)))
             else:
-                stateD[nodeID] = [False]*obsIdx + ["SOMEMAX"]
+                states = [float(x) for x in states]
+                stateD[nodeID] = [False]*obsIdx + [states.index(max(states))]
     return {"state1":state1, "state2":state2, "state3":state3, "stateD":stateD}
+def inchlibCSV(runPath, fileName, stateData, obsCount):
+    filePath = runPath+"/"+fileName+".csv"
+    csv = open(filePath, "w")
+    csv.write(reduce(lambda prev,curr: prev+",Observation "+str(curr), range(obsCount), "id"))
+    csv.write("\n")
+    for nodeID, obsStates in stateData.items():
+        csv.write((
+            str(nodeID)+
+            reduce(lambda prev,curr: prev+","+str(curr),obsStates,"")+
+            "\n"))
+    csv.close()
 
 @app.register("inchlib.cluster")
 def inchlibCluster(heatmapData):
-    self.log.info("inchlibCluster")
+    obsCount = len(heatmapData["posteriorProbsData"]["posteriorProbabilitiesSet"])
     # Group into state1,state2,state3,stateDominant
-    states = sortHeatmapData(heatmapData["posteriorProbsData"])
+    states = sortHeatmapData(heatmapData)
 
     cwd = os.getcwd()
     tempID = str(uuid.uuid4())
@@ -176,12 +183,26 @@ def inchlibCluster(heatmapData):
     runPath = tempPath + tempID
     os.mkdir(runPath)
 
-    obsCount = len(posteriorProbsData)
+    c = inchlib_clust.Cluster()
+    clustered = {}
     for filename, stateData in states.items():
         inchlibCSV(runPath, filename, stateData, obsCount)
         # c = inchlib_clust.Cluster()
-
-    return {}
+        filePath = runPath+"/"+filename+".csv"
+        outputPath = runPath+"/"+filename+".json"
+        system_call("python inchlib_clust.py "+filePath+"-o "+runPath+"/"+filename+".json -dh")
+        with open(outputPath) as clustOutput:
+            inchlibJSON = json.load(clustOutput)
+        clustered[filename] = inchlibJSON
+        # c.read_csv(filePath, ",", True, False, "numeric")
+        # # c.read_csv(filePath, ",", True, False)
+        # c.normalize_data((0,1), False)
+        # c.cluster_data("euclidean", "single", "row", "euclidean", "ward")
+        # print c
+        # d = inchlib_clust.Dendrogram(c)
+        # print d
+        # clustered[filename] = d
+    return clustered
 
 # Start
 if __name__ == "__main__":
