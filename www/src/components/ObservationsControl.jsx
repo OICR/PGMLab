10f329a57831e5caf6ed2343900aa5a6bbc5422b
Martin Pham <mdpham@uwaterloo.ca>
2016-06-15 10:56:19 -0400

    Pathways/ObservationsControl: removed render helper functions

--- www/src/components/ObservationsControl.jsx
+++ www/src/components/ObservationsControl.jsx
@@ -9,16 +9,10 @@ var classNames = require("classnames");
 export class ObservationsControl extends React.Component {
   constructor(props){
     super(props);
-
     this.state = {
       nodeFilterText: ""
     }
-    this.nodeFilterTextUpdate = this.nodeFilterTextUpdate.bind(this);
     this.handleObservationChange = this.handleObservationChange.bind(this);
-    this.header = this.header.bind(this);
-  }
-  nodeFilterTextUpdate(){
-    this.setState({"nodeFilterText": this.refs["nodeFilterInput"].value});
   }
   handleObservationChange(newActiveIndex){
     if (this.props.observationMap.get("Current").get("Active Observation") !== newActiveIndex) {
@@ -27,36 +21,29 @@ export class ObservationsControl extends React.Component {
   }
 
   // RENDERING
-  header(){
+  render(){
     let observationMap = this.props.observationMap;
     const activeObservationPosn = observationMap.get("Current").get("Active Observation");
     const selectedObservations = observationMap.get("Current").get("Selected Observations");
     const observationItems = [...selectedObservations.values()].map(posn =>
       <MenuItem key={posn} value={posn} primaryText={`Observation ${posn}`} />
     );
-    // const noPad={paddingBottom:"0px", paddingTop:"0px"};
-    const noPad={};
-    return (
-      <div className="center-align">
-        <div className="chip grey lighten-5">Inspect an observation and its node states</div>
-        <SelectField  value={activeObservationPosn}
-                      onChange={(evt,idx,val)=>{this.handleObservationChange(val)}}
-                      autoWidth={true}
-                      style={{width:"100%"}}
-                      children={observationItems} />
-        <div className="collection-item" style={noPad}>
-          <input type="text" ref="nodeFilterInput" placeholder="Type to filter nodes"
-            value={this.state.nodeFilterText} onChange={this.nodeFilterTextUpdate} />
-        </div>
-      </div>
-    );
-  }
-  render(){
     const noPad={paddingBottom:"0px", paddingTop:"0px"};
     return (
       <div id="ObservationsControl" className="section" style={noPad}>
         <div className="card-panel">
-          {this.header()}
+          <div className="center-align">
+            <div className="chip grey lighten-5">Inspect an observation and its node states</div>
+            <SelectField  value={activeObservationPosn}
+                          onChange={(evt,idx,val)=>{this.handleObservationChange(val)}}
+                          autoWidth={true}
+                          style={{width:"100%"}}
+                          children={observationItems} />
+            <div className="collection-item" style={noPad}>
+              <input type="text" ref="nodeFilterInput" placeholder="Type to filter nodes"
+                value={this.state.nodeFilterText} onChange={evt=>{this.setState({nodeFilterText:evt.target.value})}} />
+            </div>
+          </div>
           <NodeList activeType="Observation"
                     pairwiseInteractions={this.props.pairwiseInteractions}
                     observationMap = {this.props.observationMap}
--- www/src/components/PathwaysControl.jsx
+++ www/src/components/PathwaysControl.jsx
@@ -7,16 +7,10 @@ import {SelectField, MenuItem} from "material-ui";
 export class PathwaysControl extends React.Component {
   constructor(props){
     super(props);
-
     this.state = {
       nodeFilterText: ""
     }
-    this.nodeFilterTextUpdate = this.nodeFilterTextUpdate.bind(this);
     this.handlePathwayChange = this.handlePathwayChange.bind(this);
-    this.header = this.header.bind(this);
-  }
-  nodeFilterTextUpdate(){
-    this.setState({"nodeFilterText": this.refs["nodeFilterInput"].value});
   }
   handlePathwayChange(pathwayID){
     if (this.props.pathwayMap.get("Active").id !== pathwayID) {
@@ -25,36 +19,29 @@ export class PathwaysControl extends React.Component {
   }
 
   // RENDERING
-  header(){
+  render(){
     let pathwayMap = this.props.pathwayMap;
     const pathwayItems = [...pathwayMap.get("Selected").values()].map(pathway =>
       <MenuItem key={pathway.id} value={pathway.id}
                 primaryText={`${pathway.name}`} label={`${pathway.name}`} />
     );
-    // const noPad={paddingBottom:"0px", paddingTop:"0px"};
-    const noPad={};
-    return (
-      <div className="center-align">
-        <div className="chip grey lighten-5">Inspect a pathway and its node states</div>
-        <SelectField  value={pathwayMap.get("Active").id}
-                      onChange={(evt,idx,val)=>{this.handlePathwayChange(val)}}
-                      autoWidth={true}
-                      style={{width:"100%"}}
-                      children={pathwayItems} />
-        <div className="collection-item" style={noPad}>
-          <input type="text" ref="nodeFilterInput" placeholder="Type to filter nodes"
-            value={this.state.nodeFilterText} onChange={this.nodeFilterTextUpdate} />
-        </div>
-      </div>
-    );
-  }
-  render(){
     // console.log(this.props);
     const noPad={paddingBottom:"0px", paddingTop:"0px"};
     return (
       <div id="PathwaysControl" className="section" style={noPad}>
         <div className="card-panel">
-          {this.header()}
+          <div className="center-align">
+            <div className="chip grey lighten-5">Inspect a pathway and its node states</div>
+            <SelectField  value={pathwayMap.get("Active").id}
+                          onChange={(evt,idx,val)=>{this.handlePathwayChange(val)}}
+                          autoWidth={true}
+                          style={{width:"100%"}}
+                          children={pathwayItems} />
+            <div className="collection-item" style={noPad}>
+              <input type="text" ref="nodeFilterInput" placeholder="Type to filter nodes"
+                value={this.state.nodeFilterText} onChange={(evt)=>{this.setState({nodeFilterText:evt.target.value})}} />
+            </div>
+          </div>
           <NodeList activeType="Pathway"
                     pairwiseInteractions={this.props.pairwiseInteractions}
                     observationMap={this.props.observationMap}
