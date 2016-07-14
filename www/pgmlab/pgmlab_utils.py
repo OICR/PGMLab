9bdce2015df935e0d61fcf69a5ee83dc7a1bb2fa
Martin Pham <mdpham@uwaterloo.ca>
2016-07-14 13:53:03 -0400

    collect meta info over task to write info.txt file

--- www/pgmlab/pgmlab_utils.py
+++ www/pgmlab/pgmlab_utils.py
@@ -24,12 +24,12 @@ def write_observation(run_path, obs_file, run_type):
     obs.close()
 
 def generate_logical_factorgraph(run_path):
+    starttime = datetime.datetime.now()
     command = "{0} --generate-factorgraph --pairwise-interaction-file={1}pathway.pi --logical-factorgraph-file={1}logical.fg --number-of-states 3".format(pgmlab_path, run_path)
-    return system_call(command)
-
-def learning(run_path, number_states, log_likelihood_change_limit, em_max_iterations):
-    command = "{0} --learning --pairwise-interaction-file={1}pathway.pi --logical-factorgraph-file={1}logical.fg --learning-observed-data-file={1}learning.obs --estimated-parameters-file={1}learnt.fg --number-of-states {2} --log-likelihood-change-limit={3} --em-max-iterations={4}".format(pgmlab_path,run_path,number_states,log_likelihood_change_limit,em_max_iterations)
-    return system_call(command)
+    yield command # return command used to generate logical factograph
+    yield system_call(command) # then generate it
+    yield str(datetime.datetime.now() - starttime) # then take time difference for runtime
+    # return system_call(command)
 
 def write_learnt_factorgraph(run_path, lfg_file):
     lfg_filepath = run_path+"learnt.fg"
@@ -37,20 +37,34 @@ def write_learnt_factorgraph(run_path, lfg_file):
     lfg.write(lfg_file)
     lfg.close()
 
+def learning(run_path, number_states, log_likelihood_change_limit, em_max_iterations):
+    starttime = datetime.datetime.now()
+    command = "{0} --learning --pairwise-interaction-file={1}pathway.pi --logical-factorgraph-file={1}logical.fg --learning-observed-data-file={1}learning.obs --estimated-parameters-file={1}learnt.fg --number-of-states {2} --log-likelihood-change-limit={3} --em-max-iterations={4}".format(pgmlab_path,run_path,number_states,log_likelihood_change_limit,em_max_iterations)
+    yield command
+    yield system_call(command)
+    yield str(datetime.datetime.now() - starttime)
+    # return system_call(command)
+
 def inference(run_path, number_states, fg="logical.fg"):
+    starttime = datetime.datetime.now()
     command = "{0} --inference --pairwise-interaction-file={1}pathway.pi --inference-factorgraph-file={1}{2} --inference-observed-data-file={1}inference.obs --posterior-probability-file={1}pathway.pp --number-of-states {3}".format(pgmlab_path, run_path, fg, number_states)
-    return system_call(command)
+    yield command
+    yield system_call(command)
+    yield str(datetime.datetime.now() - starttime)
+    # return system_call(command)
 
 # Task pipeline:
 #  Pairwise, Observations, Factorgraph
 #  Learning || Inference
 #  Package to download
-def write_info(run_path):
-    info_filepath = run_path+"info.txt"
+def write_info_file(run_path, meta_info):
+    filepath = run_path+"info.txt"
+    print(filepath, meta_info)
 
 # LEARNING
 def learning_task(task_id, run_path, package_path, kwargs):
     os.mkdir(run_path)
+    meta_info = {"task_id":task_id,"task_kwargs":kwargs}
     # Pairwise interaction
     pi_file = kwargs["pi_file"]
     write_pairwise_interaction(run_path, pi_file)
@@ -59,22 +73,32 @@ def learning_task(task_id, run_path, package_path, kwargs):
     run_type = kwargs["task_type"]
     write_observation(run_path, obs_file, run_type)
     # Factorgraph
-    return_code = generate_logical_factorgraph(run_path)
-    if return_code != "0":
+    lfg_generator = generate_logical_factorgraph(run_path)
+    meta_info["lfg_command"] = next(lfg_generator)
+    lfg_return_code = next(lfg_generator)
+    meta_info["lfg_runtime"]= next(lfg_generator)
+    # return_code = generate_logical_factorgraph(run_path)
+    if lfg_return_code != "0":
         # self.update_state(state=states.FAILURE, meta="reason for failure")
         raise InvalidTaskError()
     # Learning
     number_states = kwargs["number_states"]
     change_limit = kwargs["change_limit"]
     max_iterations = kwargs["max_iterations"]
-    return_code = learning(run_path, number_states, change_limit, max_iterations)
-    if return_code != "0":
+    learning_generator = learning(run_path, number_states, change_limit, max_iterations)
+    meta_info["pgm_command"] = next(learning_generator)
+    pgm_return_code = next(learning_generator)
+    meta_info["pgm_runtime"] = next(learning_generator)
+    # pgm_return_code = learning(run_path, number_states, change_limit, max_iterations)
+    if pgm_return_code != "0":
         raise InvalidTaskError()
     # Package to down
+    write_info_file(run_path, meta_info)
     shutil.make_archive(package_path, "zip", root_dir=run_path)
 # INFERENCE
 def inference_task(task_id, run_path, package_path, kwargs):
     os.mkdir(run_path)
+    meta_info = {"task_id":task_id,"task_kwargs":kwargs}
     # Pairwise Interaction
     pi_file = kwargs["pi_file"]
     write_pairwise_interaction(run_path, pi_file)
@@ -83,9 +107,14 @@ def inference_task(task_id, run_path, package_path, kwargs):
     run_type = kwargs["task_type"]
     write_observation(run_path, obs_file, run_type)
     # Logical (|| Learnt) Factorgraph
-    return_code = generate_logical_factorgraph(run_path)
-    if return_code != "0":
+    lfg_generator = generate_logical_factorgraph(run_path)
+    meta_info["lfg_command"] = next(lfg_generator)
+    lfg_return_code = next(lfg_generator)
+    meta_info["lfg_runtime"] = next(lfg_generator)
+    # return_code = generate_logical_factorgraph(run_path)
+    if lfg_return_code != "0":
         raise InvalidTaskError()
+    # If a learnt factorgraph is provided, use it for inference instead
     lfg_file = kwargs["lfg_file"]
     if lfg_file != "":
         write_learnt_factorgraph(run_path, lfg_file)
@@ -94,8 +123,13 @@ def inference_task(task_id, run_path, package_path, kwargs):
         fg_name = "logical.fg"
     # Inference
     number_states = kwargs["number_states"]
-    return_code = inference(run_path, number_states, fg_name)
-    if return_code != "0":
+    inference_generator = inference(run_path, number_states, fg_name)
+    meta_info["pgm_command"] = next(inference_generator)
+    pgm_return_code = next(inference_generator)
+    meta_info["pgm_runtime"] = next(inference_generator)
+    # pgm_return_code = inference(run_path, number_states, fg_name)
+    if pgm_return_code != "0":
         raise InvalidTaskError()
     # Package
+    write_info_file(run_path, meta_info)
     shutil.make_archive(package_path, "zip", root_dir=run_path)
