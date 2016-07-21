ae41f380b6fdf811ec17144e829c10aec00d60ab
Martin Pham <mdpham@uwaterloo.ca>
2016-07-21 16:34:31 -0400

    Split Learning/InferenceSubmit components

--- www/src/components/PGMLab/LearningSubmit.jsx
+++ www/src/components/PGMLab/LearningSubmit.jsx
@@ -32,59 +32,63 @@ export default class LearningSubmit extends React.Component {
   }
   render(){
     const noMarginTop = {marginTop: "0px"};
+    // Uploading .pi and .obs files
+    const uploads = {
+      "pairwiseInteraction": "Pairwise interaction File",
+      "observation": "Observation File"
+    };
+    const uploadInputs = (
+      Object.keys(uploads)
+        .map(k => (
+          <div key={k} className="row input-field file-field">
+            <h6 className="center-align">{`${uploads[k]}`}</h6>
+            <div className="btn">
+              <span><i className="material-icons">file_upload</i></span>
+              <input type="file" name={`${k}File`} />
+            </div>
+            <div className="file-path-wrapper">
+              <input
+                type="text" className="file-path validate"
+                name={`${k}Filename`} placeholder="Upload a file" />
+            </div>
+          </div>
+        ))
+    );
+    // Parameters
+    const numberStatesInput = (
+      <div className="row input-field">
+        <span>{`Number of States: ${this.state.numberStates}`}</span>
+        <p className="range-field" style={noMarginTop}>
+          <input  type="range" name="numberStates" defaultValue={3} min={2} max={10}
+                  onInput={evt=>{this.setState({numberStates: evt.target.value})}} />
+        </p>
+      </div>
+    );
+    const maxIterationInput = (
+      <div className="row input-field">
+        <span>{`EM Max Iterations: ${this.state.maxIterations}`}</span>
+        <p className="range-field" style={noMarginTop}>
+          <input type="range" name="emMaxIterations" defaultValue={4000} min={0} max={8000}
+            onChange={evt=>{this.setState({maxIterations: evt.target.value})}} />
+        </p>
+      </div>
+    );
+    const logLikelihoodInput = (
+      <div className="row input-field">
+        <span>{"Log Likelihood Change Limit:"}</span>
+        <input type="number" name="logLikelihoodChangeLimit" step={0.00001} defaultValue={0.001} min={0}
+          className="center-align"/>
+      </div>
+    );
     return (
       <form ref="learningForm" className="center-align" onSubmit={evt=>{this.submitLearning(evt)}}>
-          <div className="row input-field file-field">
-            <p style={{textAlign:"center"}}>{"Pairwise interaction File"}</p>
-            <div className="btn">
-              <span><i className="material-icons">file_upload</i></span>
-              <input type="file" name="pairwiseInteractionFile"/>
-            </div>
-            <div className="file-path-wrapper">
-              <input
-                type="text" className="file-path validate"
-                name="pairwiseInteractionFilename"
-                placeholder="Upload a file" />
-            </div>
-          </div>
-
-          <div className="row input-field file-field">
-            <p style={{textAlign:"center"}}>{"Observation File"}</p>
-            <div className="btn">
-              <span><i className="material-icons">file_upload</i></span>
-              <input type="file" name="observationFile" placeholder="Upload a file"/>
-            </div>
-            <div className="file-path-wrapper">
-              <input
-                type="text" className="file-path validate"
-                name="observationFilename"
-                placeholder="Upload a file" />
-            </div>
-          </div>
-
-          <div className="row input-field">
-            <span>{"Number of States: "+this.state.numberStates}</span>
-            <p className="range-field" style={noMarginTop}>
-              <input  type="range" name="numberStates" defaultValue={3} min={2} max={10}
-                      onInput={evt=>{this.setState({numberStates: evt.target.value})}} />
-            </p>
-          </div>
-
-          <div className="row input-field">
-            <span>{"EM Max Iterations: "+this.state.maxIterations}</span>
-            <p className="range-field" style={noMarginTop}>
-              <input  type="range" name="emMaxIterations" defaultValue={4000} min={0} max={8000}
-                      onChange={evt=>{this.setState({maxIterations: evt.target.value})}}
-                      style={{"textAlign":"center"}}/>
-            </p>
-          </div>
-
-          <div className="row input-field">
-            <span>{"Log Likelihood Change Limit:"}</span>
-            <input type="number" name="logLikelihoodChangeLimit" step={0.00001} defaultValue={0.001} min={0}
-              style={{textAlign:"center"}}/>
-          </div>
-          <button className="waves-effect btn-large" type="submit">Run Learning</button>
+          {uploadInputs}
+          {numberStatesInput}
+          {maxIterationInput}
+          {logLikelihoodInput}
+          <button className="waves-effect btn-large" type="submit">
+            {"Run Learning"}
+          </button>
       </form>
     );
   }
