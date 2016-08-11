5e33c7e813e3dbbea2cdaa175712638211995825
Adam Wright <adam.j.wright82@gmail.com>
2016-08-11 17:55:49 -0400

    make it so that it outputs the filename and whether or not it is a DAG

---
+++ bin/process_reactome_raw_pathways.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use autodie;
+use feature qw(say);
+use POSIX;
+
+use Data::Dumper;
+
+use FindBin qw($Bin);
+use lib "$Bin/../lib/perl";
+
+use PGMLab qw(is_pi_DAG create_pi_file get_interactions_in_pi_file get_siblings_in_pi_file);
+use PGMLab::NetworkComponents qw(network_components);
+
+my $reactome_export_dir = $ARGV[0];
+$reactome_export_dir = $1 if($reactome_export_dir=~/(.*)\/$/); #remove trailing slash if exists
+
+my $reactome_pi_result_dir = $ARGV[1];
+$reactome_pi_result_dir = $1 if($reactome_pi_result_dir=~/(.*)\/$/);
+
+opendir(my $dir_h, $reactome_export_dir);
+my @tsv_files = grep(/\.tsv$/,readdir($dir_h));
+closedir($dir_h);
+
+foreach my $tsv_file (@tsv_files) {
+    my ($pathway_name, $extension) = split /\./, $tsv_file;
+    $tsv_file = "$reactome_export_dir/$tsv_file";
+    my $interactions = get_interactions_in_pi_file($tsv_file);
+    my $siblings = get_siblings_in_pi_file($tsv_file);
+    
+    my $member_groups = network_components($siblings);
+    
+    my %member_hash =();
+
+    my $member_group_id = 0;
+    foreach my $members (@{$member_groups}) {
+        $member_hash{"group_$member_group_id"} = $member_groups->[$member_group_id];
+        $member_group_id++;
+    }
+
+    my $member_group_index = 1;
+    foreach my $key (sort {$member_hash{$b} <=> $member_hash{$a}} keys %member_hash) {
+       my $members = $member_hash{$key};
+ 
+       my %member_interactions = ();
+       foreach my $node (@{$members}) {
+           $member_interactions{$node} = $interactions->{$node};
+       }   
+
+       my $filepath = "$reactome_pi_result_dir/$pathway_name";
+       $filepath .= "_$member_group_index" if ($member_group_index != 1);
+       $filepath .= ".pi";
+      
+       say $filepath;
+       my $is_a_dag = (is_pi_DAG(\%member_interactions))? "yes":"no";
+       say "DAG? $is_a_dag";
+
+       create_pi_file($filepath, \%member_interactions);
+
+       $member_group_index++;    
+    }
+
+}
--- lib/perl/PGMLab.pm
+++ lib/perl/PGMLab.pm
@@ -6,9 +6,81 @@ use warnings;
 use autodie;
 use feature qw(say);
 
+use Data::Dumper;
+
 use base "Exporter";
 use vars qw(@EXPORT_OK);
-@EXPORT_OK = qw(create_obs_file);
+@EXPORT_OK = qw(is_pi_DAG create_pi_file create_obs_file get_nodes_in_pi_file get_interactions_in_pi_file get_siblings_in_pi_file);
+
+sub is_pi_DAG {
+    my($pi_interactions) = @_;
+
+    my %network;
+    foreach my $node (keys %{$pi_interactions}) {
+        if (defined $pi_interactions->{$node}) {
+             my @keys = keys %{$pi_interactions->{$node}};
+             my $children = \@keys;
+             if ($children) { #can't be in cycle if does not have children
+                 $network{$node} = {"children" => $children,
+                                    "visited"  => 0};
+             }
+        }
+    }
+
+    foreach my $node (keys %network) {
+        next if ($network{$node}{visited});
+        $network{$node}{visited} = $node;
+
+        my $start_node = $node;
+        my $resp = visit_children(\%network, $start_node, $node); 
+        return $resp if ($resp);
+    }
+
+    return 0;
+}
+
+sub visit_children {
+    my ($network, $start_node, $node) = @_;
+
+    foreach my $child (@{$network->{$node}{children}}) {
+         return 1 if (($network->{$child}{visited}) && ($network->{$child}{visited} eq $start_node)); 
+         $network->{$node}{visited} = $start_node if (not $network->{$child}{visited});
+         my $resp = visit_children($network, $start_node, $child); 
+         return $resp if($resp);
+    }
+
+    return 0;   
+}
+
+sub create_pi_file {
+    my ($pi_filepath, $interactions) = @_;
+
+    my $number_of_interactions = get_number_of_interactions($interactions);
+
+    open(my $fh, ">", $pi_filepath);
+
+    print $fh "$number_of_interactions\n\n";
+    foreach my $child (keys %{$interactions}) {
+        foreach my $parent (keys %{$interactions->{$child}}) {
+            my @values = @{$interactions->{$child}{$parent}};
+            print $fh "$parent\t$child\t".$values[0]."\t".$values[1]."\n";
+        }
+    }
+    
+    close($fh);
+}
+
+sub get_number_of_interactions {
+    my ($interactions) = @_;
+
+    my $number_of_interactions = 0;
+    foreach my $child (keys %{$interactions}) {
+        $number_of_interactions += scalar( keys %{$interactions->{$child}});
+    }
+
+    return $number_of_interactions;
+}
+
 
 sub create_obs_file {
     my ($obs_filepath, $sample_gene_states, $sample_list) = @_;
@@ -29,6 +101,70 @@ sub create_obs_file {
     }
 
     close($obs_fh);
+}
+
+
+sub get_interactions_in_pi_file {
+    my ($pi_file_path) = @_;
+
+    open(my $fh_pi, "<", $pi_file_path);
+
+    <$fh_pi>;<$fh_pi>; ##remove fist two rows
+
+    my %pi_interactions;
+    my ($from, $to, $column_3, $column_4);
+    while (my $interaction = <$fh_pi>) {
+        chomp $interaction;
+        ($from, $to, $column_3, $column_4) = split /\t/, $interaction;
+        $pi_interactions{$from}{$to} = [$column_3, $column_4];
+    }
+
+    close($fh_pi);
+
+    return \%pi_interactions;
+}
+
+sub get_nodes_in_pi_file {
+    my ($pi_file_path) = @_;
+
+    open(my $fh_pi, "<", $pi_file_path);
+
+    <$fh_pi>;<$fh_pi>; ##remove fist two rows
+
+    my %pi_nodes;
+    my ($from, $to, $column_3, $column_4);
+    while (my $interaction = <$fh_pi>) {
+        chomp $interaction;
+        ($from, $to, $column_3, $column_4) = split /\t/, $interaction;
+        $pi_nodes{$from} = 1;
+        $pi_nodes{$to} = 1;
+
+    }
+
+    close($fh_pi);
+
+    return \%pi_nodes;
+}
+
+sub get_siblings_in_pi_file {
+    my ($pi_filepath) = @_;
+
+    open(my $fh, "<", $pi_filepath);
+
+    #skip header
+    <$fh>;<$fh>;
+
+    my %siblings;
+    while (my $line = <$fh>) {
+        chomp $line;
+        my ($from, $to, $part_three, $part_four) = split /\t/, $line;
+        $siblings{$to}{siblings}{$from} = 1;
+        $siblings{$from}{siblings}{$to} = 1;
+    }
+
+    close $fh;
+
+    return \%siblings;
 }
 
 1;
