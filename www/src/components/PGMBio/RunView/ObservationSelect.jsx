8691f2433b0f5fc4d86cff483ebfae8ef30bbd9d
Martin Pham <mdpham@uwaterloo.ca>
2016-08-16 16:05:42 -0400

    expand observationSet table if none selected

--- www/src/components/PGMBio/RunView/ObservationSelect.jsx
+++ www/src/components/PGMBio/RunView/ObservationSelect.jsx
@@ -56,7 +56,7 @@ class ObservationTable extends React.Component {
     this.props.selectObservations(selected);
   }
   render(){
-    return (!this.props.dataspace.has("observationSet") ? null :
+    return (
       <Table multiSelectable={true} height={"330px"}
           onRowSelection={selected => this.onRowSelection(selected)}>
         <TableHeader displaySelectAll={false}>
@@ -106,17 +106,20 @@ export default class ObservationSelect extends React.Component {
             title={"Select an observation set and data to include"}
             titleClassName={"center-align"}>
           <div className="row">
-            <div className={this.props.dataspace.has("observationSet") ? "col s5" : "col s12"}>
+            <div className={!this.props.dataspace.has("observationSet") ? "col s12" : "col s5"}>
               <ObservationSetTable
                   dataspace={this.props.dataspace}
                   selectObservationSet={this.props.selectObservationSet}
                   observations={this.props.observations}/>
             </div>
-            <div className="col s7">
-              <ObservationTable
-                  dataspace={this.props.dataspace}
-                  selectObservations={this.props.selectObservations}/>
-            </div>
+            {
+              !this.props.dataspace.has("observationSet") ? null :
+              <div className="col s7">
+                <ObservationTable
+                    dataspace={this.props.dataspace}
+                    selectObservations={this.props.selectObservations}/>
+              </div>
+            }
           </div>
         </Dialog>
       </div>
