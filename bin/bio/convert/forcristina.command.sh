06e535935f3a1f8adb2ff0bb3a340c545295480f
Cristina Baciu <cbaciu@olm-cbaciu.oicr.on.ca>
2016-08-18 13:07:38 -0400

    modified christina script

--- bin/bio/convert/forcristina.command.sh
+++ bin/bio/convert/forcristina.command.sh
@@ -1,4 +1,4 @@
-perl gistic_to_dominant_state_table.pl -verbose --gistic-file=/Users/cbaciu/Desktop/gist_TEAM_v3_run_3states.txt --network=Cell_Cycle_Checkpoints_patched2 --db-id-to-name-mapping=/Users/cbaciu/Desktop/db_id_to_name_mapping_05.16.txt --data-dir=/Users/cbaciu/Desktop/data_dir/ --number-of-states=3 --key-outputs-file=/Users/cbaciu/Desktop/key_outputs_cell_cycle_checkpoints
+perl gistic_to_dominant_state_table.pl -verbose --gistic-file=/Users/cbaciu/Documents/my_doc/Oncoscan_CNV/pgmlab_data_anal/CNV/GISTIC_v3_run/gist_TEAM_v3_run_3states.txt --network=Signaling_by_ERBB2_patched2 --db-id-to-name-mapping=/Users/cbaciu/Documents/my_doc/Oncoscan_CNV/pgmlab_data_anal/db_id_to_name_mapping_05.16.txt --data-dir=/Users/cbaciu/Desktop/test_gistic_team/data_dir/ --number-of-states=3 --key-outputs-file=/Users/cbaciu/Documents/my_doc/Oncoscan_CNV/pgmlab_data_anal/key_outputs/key_outputs_erbb2
 
 #--sample-list-file=390_donorID_transp.txt
 
