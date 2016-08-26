6635dc3b7dda47e7d488c2b4333d89f6e081a3a6
Martin Pham <mdpham@uwaterloo.ca>
2016-08-26 14:46:55 -0400

    action_types module for reducers/action_creators

--- www/src/components/PGMBio/redux/graphReducer.jsx
+++ www/src/components/PGMBio/redux/graphReducer.jsx
@@ -1,14 +1,22 @@
 import {Map} from "immutable"
 
+function changeGraphPathway(state, action) {
+  return state.setIn(["graphVis", "viewPathway"], action.payload.pathwayID)
+}
+
+function changeGraphObservation(state, action) {
+  return state.withMutations(state => state
+    .setIn(["graphVis", "viewObservation"], action.payload.obsIndex)
+    .setIn(["graphVis", "viewObservationSet"], state.getIn(["dataspace","observationSet","_id"]))
+  )
+}
+
 const graphReducer = (state=Map(), action) => {
   switch (action.payload.graphVisType) {
     case "SELECT_PATHWAY":
-      return state.setIn(["graphVis", "viewPathway"], action.payload.pathwayID)
+      return changeGraphPathway(state, action)
     case "SELECT_OBSERVATION":
-      return state.withMutations(state => state
-        .setIn(["graphVis", "viewObservation"], action.payload.obsIndex)
-        .setIn(["graphVis", "viewObservationSet"], state.getIn(["dataspace","observationSet","_id"]))
-      )
+      return changeGraphObservation(state, action)
   }
   return state
 }
