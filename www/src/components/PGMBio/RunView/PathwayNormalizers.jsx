9367465cc8f0a88c49c7328ecdfd1e1cdd676a18
Martin Pham <mdpham@uwaterloo.ca>
2016-08-26 11:30:02 -0400

    runview cleanup

--- www/src/components/PGMBio/RunView/PathwayNormalizers.jsx
+++ www/src/components/PGMBio/RunView/PathwayNormalizers.jsx
@@ -1,6 +1,6 @@
 // Functions for normalizing Reactome/User uploaded pathways
 // Reactome pathways have 'name',"id", uploaded pathways have 'filename',"_id"
-const getSource = p => pathway.has("id") ? "reactome" : "user"
+const getSource = p => p.has("id") ? "reactome" : "user"
 const getLabel = p => getName(p).length > 35 ? getName(p).substr(0,35).concat("...") : getName(p)
 const getName = p => p.has("name") ? p.get("name") : p.get("filename")
 const getID = p => p.has("id") ? p.get("id") : p.get("_id")
